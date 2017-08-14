bba7c5f420c5a45ce44c52d9197f78586d92d353
luyuheng <1213079518@qq.com>
2017-08-14 17:16:18 +0800

    finish hw8

--- 8/Party.hs
+++ 8/Party.hs
@@ -3,6 +3,9 @@ module Party where
 import Employee
 import Data.Monoid
 import Data.Tree
+import System.IO
+import Data.List
+import Data.String
 
 --e1
 glCons :: Employee -> GuestList -> GuestList
@@ -14,9 +17,38 @@ instance Monoid GuestList where
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun gs1@(GL _ f1) gs2@(GL _ f2) | f1<f2 =gs2
-                              | otherwise = gs1
+                                    | otherwise = gs1
 
 --e2
-treeFold :: b -> ([b] -> a -> b) -> Tree a -> b
-treeFold e f (Node x []) = f [] x
-treeFold e f (Node x chs) = f (map (treeFold e f) chs) x
+treeFold :: b -> (a -> [b] -> b) -> Tree a -> b
+treeFold e f (Node x []) = f x []
+treeFold e f (Node x chs) = f x (map (treeFold e f) chs)
+
+--e3
+nextLevel :: Employee
+          -> [(GuestList, GuestList)] --(with current boss, without)
+          -> (GuestList, GuestList)
+nextLevel e glps = (glCons e $ foldr mappend mempty $ map snd glps,
+                    foldr mappend mempty $ map (uncurry max) glps)
+
+--e4
+maxFun :: Tree Employee -> GuestList
+maxFun t = (uncurry max) $ treeFold (GL [] 0, GL [] 0) nextLevel t
+
+--e5
+printAll :: Tree Employee -> IO()
+printAll t = runall $ map putStrLn ss
+  where gl@(GL es f) = maxFun t
+        ss = ("Total fun:" ++ (show f)) : (map empName $ sortBy (\(Emp name1 _) (Emp name2 _)-> compare name1 name2) es)
+
+runall :: [IO()] -> IO()
+runall [] = return ()
+runall (e:es) = do
+  e
+  runall es
+
+main :: IO ()
+main = do
+  inpStr <- readFile "company.txt"
+  inpTree <- readIO inpStr
+  printAll inpTree
